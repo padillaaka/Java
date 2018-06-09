f80511cc5e47e7d3da8d752fb2f5e256ce5d16f7
Jose Manuel Padilla <jmpadillam@gmail.com>
2018-06-09 20:53:38 +0200

    Primer if

--- ComparaNumeros/src/Main.java
+++ ComparaNumeros/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String []Args){
-        System.out.println("Hola");
+        //Para leer por teclado
+        Scanner teclado = new Scanner(System.in);
+
+        //Variable para guardar la entrada de teclado
+        int numeroLeido;
+
+        //Pedimos al usuario un numero
+        System.out.print("Introduzca un numero: ");
+        numeroLeido = teclado.nextInt();
+
+        //Comparamos
+        if (numeroLeido < 0){
+            System.out.println("Su numero es negativo");
+        }
+
+        if (numeroLeido > 0 && numeroLeido <= 5){
+            System.out.println("Suspenso");
+        }
+
+        if (numeroLeido > 5 && numeroLeido <= 8){
+            System.out.println("Notable");
+        }
+
+        if (numeroLeido > 8 && numeroLeido <= 10){
+            System.out.println("Sobresaliente");
+        }
+
+        System.out.println("Su numero: "+numeroLeido);
+
+
+
+
     }
 }
